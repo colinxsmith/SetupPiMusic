64b19fb0e6ca9fd84801d155579e4a9cff82d112
Colin <colinxsmith@gmail.com>
2019-12-19 22:39:45 +0000

    use 320k instead of 250k for startup of bubbleupnpserver

--- launch.sh
+++ launch.sh
@@ -17,4 +17,4 @@ export PATH=.:${PATH}
 # -Xss256k: thread stack size. 256K reduces per-thread memory usage and may prevent "java.lang.OutOfMemoryError: unable to create new native thread" on some systems
 # -Djava.awt.headless=true: required for image transcoding to work on headless systems (eg no X-Window libraries)
 
-exec "${JAVA}" -Xss256k -Djava.awt.headless=true -Djava.net.preferIPv4Stack=true -Dfile.encoding="UTF-8" -jar BubbleUPnPServerLauncher.jar $*
+exec "${JAVA}" -Xss320k -Djava.awt.headless=true -Djava.net.preferIPv4Stack=true -Dfile.encoding="UTF-8" -jar BubbleUPnPServerLauncher.jar $*
