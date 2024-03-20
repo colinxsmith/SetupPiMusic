9b26bf31da191ce74eb780b8452e78154a7d9d90
Colin Smith <colinxsmith@gmail.com>
2024-03-20 23:25:32 +0000

    .

---
+++ pvrlist.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+./get_iplayer --pid=b097sn02 -o /home/pi/Videos
+./get_iplayer --pvr-add roddy "David Rodigan"
+./get_iplayer --pvr-add block "BLOCK PARTY"
+./get_iplayer --pvr-add Plant "The Secrets of Quantum Physics"
+./get_iplayer --pvr-add Plant "Robert Plant"
+./get_iplayer --pvr-add roddy "David Rodigan"
+./get_iplayer --pvr-add block "Block party"
+./get_iplayer --pvr-add Page "Jimmy Page"
+./get_iplayer --pvr-add jeff "Jeff Beck"
+./get_iplayer --pvr-add Peel "John Peel"
+./get_iplayer --pvr-add Gilles Gilles
+./get_iplayer --pvr-add Iggy Iggy
+./get_iplayer --pvr-add Jazz "Jazz on 3"
+./get_iplayer --pvr-add iq "Nabihah Iqbal"
+./get_iplayer --pvr-add deep "Deep Purple"
+./get_iplayer --pvr-add zep "Led Zepplin"
+./get_iplayer --pvr-add conc "6 Music Classic concert"
+./get_iplayer --pvr-add live "6 Music Live"
+./get_iplayer --pvr-list
