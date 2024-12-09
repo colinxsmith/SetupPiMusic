a9541a634adac71f085f7b048bd2984625aadcc1
Colin Smith <colinxsmith@gmail.com>
2024-12-09 15:51:12 +0000

    .

--- setupjazz.sh
+++ setupjazz.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 music=${1:-jez.mp3}
-mpc rescan
+echo $music
+#mpc rescan
 mpc play 3
+echo mpc insert $music
 mpc insert $music
-(mpc play 4;sleep 6000 ;mpc play 3) &
-echo $(ps -ax | grep "sleep 6000" | sed "s/ p.*//")
-ps -ax | grep "sleep 6000" | sed "s/ p.*//"
+mpc
+(mpc play 4;sleep 6000;mpc play 3)&
+mpc
 kill -9 $(ps -ax | grep "sleep 6000" | sed "s/ p.*//")
