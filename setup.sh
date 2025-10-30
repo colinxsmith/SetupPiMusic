6c344a4ec6bb6ff9992b8920efb71f33c2de3ee7
Colin Smith <colinxsmith@gmail.com>
2025-10-30 18:00:25 +0000

    .

--- setup.sh
+++ setup.sh
@@ -9,6 +9,7 @@ mkdir Music playlists
 mkdir Music/mp3
 git clone https://github.com/colinxsmith/sound
 git clone https://github.com/colinxsmith/Setuppimusic
+cp Setuppimusic/files.zip .
 sudo systemctl status mpd
 sudo systemctl enable mpd
 sudo systemctl restart mpd
@@ -16,6 +17,16 @@ sudo systemctl status mpd
 mpc
 cd sound
 mpc add "http://lsn.lv/bbcradio.m3u8?station=bbc_6music&bitrate=96000"
-mpc play 1
-mpc play 
-cd ~/Setuppimusic/
+unzip files.zip 
+(cd /boot/firmware/;sudo mv config.txt config.txt.old)
+(cd /boot/firmware/;sudo cp /home/pi/config.txt .)
+(cd /etc; ls mpd.conf asound.conf )
+(cd /etc;sudo mv mpd.conf mpd.conf.old)
+(cd /etc;sudo cp /home/pi/mpd.conf .)
+(cd /etc;sudo cp /home/pi/asound.conf .)
+(cd /etc; ls mpd.conf asound.conf )
+sudo systemctl status mpd
+sudo systemctl restart mpd
+sudo systemctl status mpd
+sound/findjazz 
+mpc add $(grep fm_b sound/*|sed "s/.*u://")
