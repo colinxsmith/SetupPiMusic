c63ffa6440f630c2c03b58de1171468ae527ce31
Colin Smith <colinxsmith@gmail.com>
2025-11-05 21:28:18 +0000

    .

--- setup.sh
+++ setup.sh
@@ -1,14 +1,13 @@
 sudo apt update
 sudo apt upgrade
 sudo apt install id3v2 mpc mpd git
-sudo raspi-config 
+git clone https://github.com/colinxsmith/SetupPiMusic
+git clone https://github.com/colinxsmith/sound
 sudo systemctl status bluetooth 
 sudo systemctl enable bluetooth 
 sudo rfkill unblock 0
-mkdir Music playlists
+mkdir Music playlists Pictures Videos
 mkdir Music/mp3
-git clone https://github.com/colinxsmith/sound
-git clone https://github.com/colinxsmith/Setuppimusic
 sudo systemctl status mpd
 sudo systemctl enable mpd
 sudo systemctl restart mpd
@@ -19,6 +18,8 @@ mpc add "http://lsn.lv/bbcradio.m3u8?station=bbc_6music&bitrate=320000"
 mpc play 1
 mpc play 
 mpc
+cd
+cp SetupPiMusic/files.zip .
 unzip files.zip 
 (cd /boot/firmware/;sudo mv config.txt config.txt.old)
 (cd /boot/firmware/;sudo cp /home/pi/config.txt .)
@@ -29,6 +30,7 @@ unzip files.zip
 sudo systemctl status mpd
 sudo systemctl restart mpd
 sudo systemctl status mpd
+cd
 sound/findjazz 
 mpc add $(grep fm_b sound/*|sed "s/.*u://")
 sudo apt install ffmpeg
@@ -44,22 +46,50 @@ git clone https://github.com/get-iplayer/get_iplayer.git
 echo fs.inotify.max_user_watches=524288 | sudo tee /etc/sysctl.d/40-max-user-watches.conf && sudo sysctl --system
 cd
 sudo apt install mediainfo minidlna -y
-sed -i "s/Bullseye/trixie64/g" Setuppimusic/patchminidlna 
+sed -i "s/Bullseye/trixie64/g" SetupPiMusic/patchminidlna 
 (cd /etc;sudo patch< ~/SetupPiMusic/patchminidlna)
-mkdir Pictures Videos
-(cd /etc;sudo patch< /home/pi/Setuppimusic/patchminidlna)
+(cd /etc;sudo patch< /home/pi/SetupPiMusic/patchminidlna)
 sudo systemctl status minidlna.service 
 sudo systemctl restart minidlna.service 
-cd Setuppimusic/
+cd SetupPiMusic/
 mkdir ~/bin
 gcc -O2 testpng.c -o ~/bin/imagedata
 cp ~/sound/bashrc ~/.bashrc
 cp ~/sound/bash_aliases ~/.bash_aliases
 sudo apt install -y imagemagick libexif-dev libjpeg-dev libid3tag0-dev libflac-dev libvorbis-dev libsqlite3-dev libavformat-dev gettext
 cd
-git clone https://git.code.sf.net/p/minidlna/git minidlna-git
-cp Setuppimusic/icons.c minidlana-git
 git config --global core.editor vim
 git config --global credential.helper store
 git config --global user.name "Colin Smith"
 git config --global user.email "colinxsmith@gmail.com"
+curl -L https://sourceforge.net/projects/minidlna/files/latest/download > minidlna.tar.gz
+gunzip minidlna.tar.gz 
+tar xf minidlna.tar 
+cp SetupPiMusic/icons.c minidlana-1.3.3
+mkdir picsetup
+cd picsetup/
+cp ../SetupPiMusic/makepic .
+cp ~/ak.jpg .
+./makepic 
+cp ../SetupPiMusic/icons.c .
+cd
+cp picsetup/* minidlna-1.3.3
+cd minidlna-1.3.3
+curl -L https://sourceforge.net/p/minidlna/patches/204/attachment/0001-libav.h-fix-build-with-ffmpeg-7.0.patch>patchfix
+patch < patchfix 
+./autogen.sh 
+./configure --prefix=$(pwd)
+make
+make install
+sudo mv /usr/sbin/minidlnad /usr/sbin/minidlnad.old
+sudo cp sbin/minidlnad /usr/sbin/
+sudo systemctl restart minidlna.service 
+cd
+sudo apt install vim
+SetupPiMusic/vimrc .vimrc
+sudo systemctl restart minidlna.service 
+mpc add "http://lsn.lv/bbcradio.m3u8?station=bbc_6music&bitrate=320000"
+sound/findjazz 
+mpc add $(grep fm_b sound/*|sed "s/.*u://")
+chmod 777 Music/ Videos/ Pictures/
+sudo systemctl restart minidlna.service 
